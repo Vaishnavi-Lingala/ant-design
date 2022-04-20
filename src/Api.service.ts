9b892fd7f827917e22103086e332c8a14943ba1e
Sandeep Gandham <sandeep_gandham@tecnics.com>
2022-04-20 13:35:13 -0500

    Added account id dynamically

--- src/Api.service.ts
+++ src/Api.service.ts
@@ -19,17 +19,17 @@ export default {
     },
 
     getAllPolicies(requestOptions: object) {
-		return fetch(backend_url + "/account/ooaab3ab3443/product/oprc735871d0/auth-policies", requestOptions)
+		return fetch(backend_url + "/account/"+ localStorage.getItem("accountId") + "/product/oprc735871d0/auth-policies", requestOptions)
 			.then(response => response.json());
     },
 
     getPolicyDetails(uid: string, requestOptions: object) {
-		return fetch(backend_url + "/auth-policies/" + uid, requestOptions)
+		return fetch(backend_url + "/account/ooa3329d11f1/auth-policies/" + uid, requestOptions)
 			.then(response => response.json());
     },
 
     updatePolicyDetails(uid: string, requestOptions: object) {
-		return fetch(backend_url + "/auth-policies/" + uid, requestOptions)
+		return fetch(backend_url + "/account/ooa3329d11f1/auth-policies/" + uid, requestOptions)
 			.then(res => res.json());
     },
 
