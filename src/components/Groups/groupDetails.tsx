44b9c48218437d90ebd637afc11d5fb8a3430ea7
Sandeep Gandham <sandeep_gandham@tecnics.com>
2022-04-29 09:24:56 -0500

    added users in group api call

--- src/components/Groups/groupDetails.tsx
+++ src/components/Groups/groupDetails.tsx
@@ -8,25 +8,39 @@ export default function GroupDetails(props: any) {
     const [groupDetails, setGroupDetails] = useState(props.groupDetails);
     const [users, setUsers] = useState([]);
     console.log('Group details: ', groupDetails)
+    console.log('Is default: ', groupDetails.is_default);
 
-    // useEffect(() => {
-	// 	// setLoadingDetails(true);
-    //     ApiService.getUsersInGroup(groupDetails.uid, accessToken)
-	// 	.then(data => {
-	// 		console.log('Users: ', data);
-    //         // data.forEach(group => {
-    //         //     group.key = group.uid;
-    //         // })
-    //         // setGroups(data);
-	// 		// setLoadingDetails(false);
-	// 	})
-	// }, [])
+    useEffect(() => {
+		// setLoadingDetails(true);
+        ApiService.getUsersInGroup(groupDetails.uid, accessToken)
+		.then(data => {
+			console.log('Users: ', data);
+            // data.forEach(group => {
+            //     group.key = group.uid;
+            // })
+            // setGroups(data);
+			// setLoadingDetails(false);
+		})
+	}, [])
 
     return(
         <>
-            <div className='content-header'>
-				{groupDetails.name}
-			</div>
+            <div className="content-container rounded-grey-border">
+                <div className="row-container">
+                    <div style={{ paddingTop: '20px' }}>
+                        <h6>Group name</h6>
+                    </div>
+                    <div style={{ paddingTop: '20px' }}>
+                            {groupDetails.name}
+                    </div>
+                    <div style={{ paddingTop: '20px' }}>
+                        <h6>Status</h6>
+                    </div>
+                    <div style={{ paddingTop: '20px' }}>
+                        {groupDetails.status}
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
