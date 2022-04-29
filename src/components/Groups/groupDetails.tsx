98318781439349c7da298cba5aceb6125dff8411
Sandeep Gandham <sandeep_gandham@tecnics.com>
2022-04-29 14:08:43 -0500

    Added modals for add users and groups

--- src/components/Groups/groupDetails.tsx
+++ src/components/Groups/groupDetails.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import ApiService from "../../Api.service";
+import { Divider, Table, Skeleton, Button, Modal, Col, Row, Typography } from "antd";
 
 export default function GroupDetails(props: any) {
 
@@ -7,19 +8,47 @@ export default function GroupDetails(props: any) {
     const accessToken = JSON.parse(localStorage.getItem("okta-token-storage")).accessToken.accessToken
     const [groupDetails, setGroupDetails] = useState(props.groupDetails);
     const [users, setUsers] = useState([]);
-    console.log('Group details: ', groupDetails)
-    console.log('Is default: ', groupDetails.is_default);
+    const [loadingDetails, setLoadingDetails] = useState(false);
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const { Title } = Typography;
+
+    const columns = [
+		{
+			title: 'Name',
+			dataIndex: 'user_name',
+			width: '30%'
+		},
+        {
+			title: 'Email',
+			dataIndex: 'email',
+			width: '40%'
+		}
+		
+	];
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+    
+    const handleOk = () => {
+        setIsModalVisible(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
 
     useEffect(() => {
-		// setLoadingDetails(true);
+		setLoadingDetails(true);
         ApiService.getUsersInGroup(groupDetails.uid, accessToken)
 		.then(data => {
 			console.log('Users: ', data);
-            // data.forEach(group => {
-            //     group.key = group.uid;
-            // })
-            // setGroups(data);
-			// setLoadingDetails(false);
+            data.results.forEach(user => {
+                user.key = user.uid;
+            })
+            setUsers(data.results);
+			setLoadingDetails(false);
 		})
 	}, [])
 
@@ -40,6 +69,50 @@ export default function GroupDetails(props: any) {
                         {groupDetails.status}
                     </div>
                 </div>
+                <Divider style={{ borderTop: '1px solid #d7d7dc' }} />
+                <Skeleton loading={loadingDetails}>
+                    <div style={{ width: '100%', border: '1px solid #D7D7DC', borderBottom: 'none', padding: '10px 10px 10px 25px', backgroundColor: '#f5f5f6' }}>
+						<Button type='primary' size='large' onClick={showModal}>Add Users</Button>
+					</div>
+                    <Table
+                            style={{ border: '1px solid #D7D7DC' }}
+                            showHeader={true}
+                            columns={columns}
+                            dataSource={users}   
+                            // bordered={true}
+                            pagination={{ position: [] }}
+                        />
+                </Skeleton>
+                <Modal title={<Title level={2}>Add Users</Title>} visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} width={1000}>
+                    <Row gutter={16}>
+                        <Col span={12}>
+                            <div style={{ paddingTop: '20px' }}>
+                                <h6>Not Members</h6>
+                            </div>
+                            <Table
+                                style={{ border: '1px solid #D7D7DC' }}
+                                showHeader={true}
+                                columns={columns}
+                                dataSource={[]}   
+                                // bordered={true}
+                                pagination={{ position: [] }}
+                            />
+                        </Col>
+                        <Col span={12}>
+                            <div style={{ paddingTop: '20px' }}>
+                                <h6>Members</h6>
+                            </div>
+                            <Table
+                                style={{ border: '1px solid #D7D7DC' }}
+                                showHeader={true}
+                                columns={columns}
+                                dataSource={[]}   
+                                // bordered={true}
+                                pagination={{ position: [] }}
+                            />
+                        </Col>
+                    </Row>
+                </Modal>
             </div>
         </>
     )
--- src/components/Groups/groups.tsx
+++ src/components/Groups/groups.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { Button, Skeleton, Table } from 'antd';
+import { Button, Skeleton, Table, Modal, Input, Row, Col, Typography } from 'antd';
 import ApiService from "../../Api.service";
 import GroupDetails from "./groupDetails";
 
@@ -8,8 +8,12 @@ export default function Groups() {
     const [groups, setGroups] = useState([]);
     const [loadingDetails, setLoadingDetails] = useState(false);
     const [groupDetails, setGroupDetails] = useState(undefined);
+    const [isModalVisible, setIsModalVisible] = useState(false);
     //@ts-ignore
     const accessToken = JSON.parse(localStorage.getItem("okta-token-storage")).accessToken.accessToken
+
+    const { TextArea } = Input;
+    const { Title } = Typography;
 
     const columns = [
 		{
@@ -28,6 +32,18 @@ export default function Groups() {
 			)
 		}
 	];
+
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+    
+    const handleOk = () => {
+        setIsModalVisible(false);
+    };
+
+    const handleCancel = () => {
+        setIsModalVisible(false);
+    };
 
     function getGroup(uid: any) {
 		setLoadingDetails(true);
@@ -60,7 +76,7 @@ export default function Groups() {
             <Skeleton loading={loadingDetails}>
                 {groupDetails ? <GroupDetails groupDetails={groupDetails}/> : <>
                     <div style={{ width: '100%', border: '1px solid #D7D7DC', borderBottom: 'none', padding: '10px 10px 10px 25px', backgroundColor: '#f5f5f6' }}>
-						<Button type='primary' size='large'>Add New Group</Button>
+						<Button type='primary' size='large'  onClick={showModal}>Add New Group</Button>
 					</div>
                     <Table
                         style={{ border: '1px solid #D7D7DC' }}
@@ -73,6 +89,37 @@ export default function Groups() {
                 </>
                 }
             </Skeleton>
+            <Modal title={<Title level={2}>Add Group</Title>} visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} width={500}>
+                <Row gutter={16}>
+                    <Col span={6}>
+                        <h6>Name</h6>
+                    </Col>
+                    <Col span={18}>
+                        <span style={{ paddingRight: '20px' }}>
+                            
+                            <Input
+                                name="groupName"
+                                type="text"
+                                className="form-control"
+                            /> 
+                            
+                        </span>
+                    </Col>
+                    <Col span={6}>
+                        <h6>Description</h6>
+                    </Col>
+                    <Col span={18}>
+                        <span style={{ paddingRight: '20px' }}>
+                            
+                            <TextArea
+                                name="description"
+                                className="form-control"
+                            /> 
+                            
+                        </span>
+                    </Col>
+                </Row>
+            </Modal>
         </>
     ) 
 }
