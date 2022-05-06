1301ffa0bb58f510c812a6334b7028379658c349
Sandeep Gandham <sandeep_gandham@tecnics.com>
2022-05-06 11:10:07 -0500

    Added search for add users functionality

--- src/components/Groups/UsersSelection.tsx
+++ src/components/Groups/UsersSelection.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from "react";
-import {Table, Skeleton, Button, Modal, Col, Row, Typography } from "antd";
+import {Table, Skeleton, Button, Modal, Col, Row, Typography, Input } from "antd";
+import ApiService from "../../Api.service";
+import ApiUrls from '../../ApiUtils';
 
 export default function UsersSelection(props: any) {
 
     const { Title } = Typography;
+    const { Search } = Input;
     const columns = [
 		{
 			title: 'Name',
@@ -19,6 +22,12 @@ export default function UsersSelection(props: any) {
 	];
 
     const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+    const [usersList, setUsersList] = useState([]);
+    const [loadingDetails, setLoadingDetails] = useState(false);
+
+    useEffect(() => {
+		setUsersList(props.users)
+	}, [])
     
     const handleOk = () => {
         props.handleOk(selectedRowKeys, props.action);
@@ -27,6 +36,7 @@ export default function UsersSelection(props: any) {
 
     const handleCancel = () => {
         setSelectedRowKeys([]);
+        setUsersList([]);
         props.handleCancel(props.action);
     };
 
@@ -40,6 +50,26 @@ export default function UsersSelection(props: any) {
         onChange: onSelectChange,
       };
 
+    const onSearch = searchText => {
+        if (props.action === 'Add') {
+            console.log('Search text: ', searchText);
+            console.log('Length: ', searchText.length);
+            setLoadingDetails(true);
+            const params = {
+                q : searchText
+            }
+            ApiService.get(ApiUrls.users, params).then(data => {
+                console.log('Data: ', data)
+                data.results.forEach(user => {
+                    user.key = user.uid;
+                })
+                setUsersList(data.results);
+                setLoadingDetails(false);
+            })
+        }
+        
+    }
+
     return(
         <>
             <Modal title={<Title level={2}>{props.action} Users</Title>} visible={true} onOk={handleOk} onCancel={handleCancel} width={1000}
@@ -51,11 +81,18 @@ export default function UsersSelection(props: any) {
                        {props.action}
                     </Button>
                 ]}>
+                <div style={{ width: '100%', border: '1px solid #D7D7DC', borderBottom: 'none', padding: '10px 10px 10px 25px', backgroundColor: '#f5f5f6' }}>
+                    <Search placeholder="Search for users by name or email" allowClear style={{ width: 400 }} 
+                    onSearch={onSearch}
+                    size="large"
+                    enterButton/>
+                </div>
                 <Table
+                    loading={loadingDetails}
                     style={{ border: '1px solid #D7D7DC' }}
                     showHeader={true}
                     columns={columns}
-                    dataSource={props.users}   
+                    dataSource={usersList}   
                     rowSelection={rowSelection}
                     // bordered={true}
                     pagination={{ position: [] }}
