19b3da4ae18c2b3e25e09f3b06ec5e9ab7fd3a15
r_allada <rallada@credenti.io>
2022-05-12 18:11:47 +0530

    Resolved the issue with nested components

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -2,15 +2,16 @@ import { useOktaAuth } from "@okta/okta-react";
 import { Redirect, Route, useHistory } from "react-router-dom";
 import { SecureRoute } from "@okta/okta-react";
 
-import Layout from "./Layout/Layout";
-
 // @ts-ignore
-export default function ProtectedRoute({ component: Component, ...restOfProps }) {
+export default function ProtectedRoute({
+    component: Component,
+    ...restOfProps
+}) {
     const { authState, oktaAuth } = useOktaAuth();
     const history = useHistory();
     const oktaStorage = localStorage.getItem("okta-token-storage");
-    
-    function removeItems(){
+
+    function removeItems() {
         localStorage.removeItem("domain");
         localStorage.removeItem("clientId");
         localStorage.removeItem("issuer");
@@ -21,31 +22,34 @@ export default function ProtectedRoute({ component: Component, ...restOfProps })
         history.push("/");
     }
 
-    if(oktaStorage !== null && oktaStorage != "") {
-        if(oktaStorage !== "{}"){
+    if (oktaStorage !== null && oktaStorage != "") {
+        if (oktaStorage !== "{}") {
             const idToken = JSON.parse(oktaStorage).idToken;
             localStorage.setItem("clientId", idToken.clientId);
             localStorage.setItem("issuer", idToken.issuer);
-        }
-        else{
+        } else {
             removeItems();
         }
-    }
-    else{
+    } else {
         removeItems();
     }
 
     return (
         <Route
             {...restOfProps}
-            render={
-                (props) => oktaStorage !== null ?
+            render={(props) =>
+                oktaStorage !== null ? (
                     <SecureRoute>
-                            <Layout>
-                                <Component authStatus={authState} oktaAuth={oktaAuth} {...props} />
-                            </Layout>
-                    </SecureRoute> : <Redirect to={"/"} />
+                        <Component
+                            authStatus={authState}
+                            oktaAuth={oktaAuth}
+                            {...props}
+                        />
+                    </SecureRoute>
+                ) : (
+                    <Redirect to={"/"} />
+                )
             }
         />
-    )
+    );
 }
