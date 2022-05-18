c12cf030375cb567385254ecb98ee91388d796c0
Chanikya.T <chanikya_telanakula@tecnics.com>
2022-05-18 22:12:54 +0530

    Added icon for each and every menu

--- src/components/Layout/Sider.tsx
+++ src/components/Layout/Sider.tsx
@@ -1,6 +1,8 @@
 import { Menu } from "antd";
 import Sider from "antd/lib/layout/Sider";
 import { useHistory } from "react-router-dom";
+import { UserOutlined, DesktopOutlined, TeamOutlined, SettingOutlined, PieChartOutlined, 
+	AreaChartOutlined, SolutionOutlined, DatabaseOutlined } from '@ant-design/icons';
 
 function AppSider() {
 	const history = useHistory();
@@ -18,15 +20,15 @@ function AppSider() {
 				selectedKeys={[window.location.pathname.split("/")[1]]}
 				className="sider-menu"
 			>
-				<Menu.Item key="dashboard">Dashboard</Menu.Item>
-				<Menu.Item key="mechanism">Mechanisms</Menu.Item>
-				<Menu.Item key="policies" >Policies</Menu.Item>
+				<Menu.Item key="dashboard"><AreaChartOutlined style={{ position: 'relative', bottom: '3px' }}/> Dashboard</Menu.Item>
+				<Menu.Item key="mechanism"><DatabaseOutlined style={{ position: 'relative', bottom: '3px' }}/> Mechanisms</Menu.Item>
+				<Menu.Item key="policies"><SolutionOutlined style={{ position: 'relative', bottom: '3px' }}/> Policies</Menu.Item>
 				{/* <Menu.Item key="config">Configuration</Menu.Item> */}
-				<Menu.Item key="users">Users</Menu.Item>
-				<Menu.Item key="machines">Machines</Menu.Item>
-				<Menu.Item key="groups">Groups</Menu.Item>
-				<Menu.Item key="activityLogs" >Activity Logs</Menu.Item>
-				<Menu.Item key="settings" >Settings</Menu.Item>
+				<Menu.Item key="users"><UserOutlined style={{ position: 'relative', bottom: '3px' }}/> Users</Menu.Item>
+				<Menu.Item key="machines"><DesktopOutlined style={{ position: 'relative', bottom: '3px' }}/> Machines</Menu.Item>
+				<Menu.Item key="groups"><TeamOutlined style={{ position: 'relative', bottom: '3px' }}/> Groups</Menu.Item>
+				<Menu.Item key="activityLogs"><PieChartOutlined style={{ position: 'relative', bottom: '3px' }}/> Activity Logs</Menu.Item>
+				<Menu.Item key="settings"><SettingOutlined style={{ position: 'relative', bottom: '3px' }}/> Settings</Menu.Item>
 			</Menu>
 		</Sider>
 	);
