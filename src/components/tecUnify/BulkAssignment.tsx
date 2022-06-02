d45ebbb124c107b1f58b253c5fcd6edd8bfcfc06
Cody Diamond <cody_diamond@tecnics.com>
2022-06-02 16:28:53 -0500

    New Types to match API requests, Styled and fleshed out BulkAssignments page.

--- src/components/tecUnify/BulkAssignment.tsx
+++ src/components/tecUnify/BulkAssignment.tsx
@@ -1,11 +1,15 @@
 // TODO: Call API to get list of all Applications
 // TODO: Virtual List, grab items as we scroll down the list
 
-import { useState, useEffect, useContext } from 'react';
-import { Checkbox } from 'antd';
+import { useState, useEffect } from 'react';
+import { Button, Checkbox, List, Input } from 'antd';
+import { CaretDownOutlined } from '@ant-design/icons';
 
 import ApiService from "../../Api.service"
 import ApiUrls from '../../ApiUtils';
+
+import type { ApiResponse, User } from './types'; 
+import type { CheckboxChangeEvent } from 'antd/es/checkbox';
 
 interface PageState {
   current: number;
@@ -17,41 +21,139 @@ const initialPState: PageState = {
   limit: 10
 };
 
-function BulkAssignment(): JSX.Element {
+const { Search } = Input;
+
+function ProperCase(s: string) { return s.charAt(0).toUpperCase() + s.substring(1).toLowerCase(); }
+
+function BulkAssignment() {
   const [page, setPage] = useState<PageState>(initialPState);
-  const [userList, setUserList] = useState();
+  const [selectedUid, setSelectedUid] = useState<(string | undefined)[]>([]);
+  const [userList, setUserList] = useState<ApiResponse>();
+  const [filteredUsers, setFilteredUsers] = useState<ApiResponse>();
 
 	async function getUsersList({ current, limit }: { current: number; limit: number; }) {
-		let data = await ApiService.get(ApiUrls.users, { start: current, limit: limit }).catch(error => {
-			console.error('Error: ', error);
-		});
+		let data = await ApiService
+      .get(ApiUrls.users, { start: current, limit: limit })
+      .catch(error => console.error('Error: ', error));
 
-		console.log(data);
 		setUserList(data);
+    setFilteredUsers(data);
 	}
 
+  useEffect(() => { 
+    getUsersList(page); 
+  },[page]);
+
+  function handleReset() {
+    setFilteredUsers(userList);
+  }
+
+
+  function filterUsers(searchVal: string) {
+    console.log(searchVal)
+  }
+
+  function sortUsers(e: any) {
+    switch(e.target.outerText) {
+      case 'Users':
+      case 'Applications':
+      default:
+    }
+  }
+
+  function handleCheckboxChange(e: CheckboxChangeEvent) {
+    setSelectedUid(current => [...current, e.target.id]);
+  }
+
+  function UserListComponent(user: User) {
+  }
+
+  function ListItem(user: User) {
+    return (
+    <List.Item className='user-select-list'>
+      <Checkbox onChange={handleCheckboxChange} id={user.uid} style={{alignSelf: 'center'}}/>
+      <h4 style={{alignSelf: 'center', marginBottom: '0px', marginRight: 'auto'}}>{user.user_name}</h4>
+      <h4 style={{alignSelf: 'center', marginBottom: '0px'}}>{ProperCase(user.status)}</h4>
+    </List.Item>
+    );
+  }
+ 
+  const ResetButton = <Button onClick={handleReset} type='link' size='small'>reset</Button>;
+  
+  function ListHeader({leftText = "", rightText = ""}) {
+    return(
+      <>
+      <Search
+        onSearch={val => console.log(val)}
+        style={{padding: '5px'}}
+        size='small'
+        width='fill'
+        placeholder='Search for Users'
+      />
+      
+      <div className='list-header-container'>
+        <span style={{cursor: 'pointer'}} onClick={sortUsers}>
+          {leftText}
+            <CaretDownOutlined/>
+        </span>
+        <span>
+          {rightText}
+        </span>
+      </div>
+      </>
+    );
+  };
+
+
   return (
-    <>
-      <div className='bulk-container'>
-        <div className='sub-header border-bottom'>
-          <span>Applications</span>
-          <a>reset</a>
+    <div className='bulk-component'>
+      <Button style={{alignSelf: 'end', margin: '5px 5px 0px'}}type='primary' size='middle'>Next</Button>
+      <div className='list-container-group'>
+        <div className='list-container'>
+          <div className='sub-header border-bottom'>
+            <span>Applications()</span>
+            {ResetButton}
+          </div>
+          <ListHeader leftText="Applications"/>
+
+
+          {/*
+          <List 
+            itemLayout='vertical'
+            pagination={{
+              onChange: page => setPage(currPage => {return {...currPage, current: page}}),
+              pageSize: page.limit,
+              total: filteredUsers?.total_items, 
+              size: 'small'
+            }}
+            header={SearchBar}
+            dataSource={filteredUsers?.results}
+            renderItem={user => <UserListComponent {...user}/>}
+            />
+          */}
         </div>
 
-        <div>
+        <div className='list-container'>
+          <div className='sub-header border-bottom'>
+            <span>Users()</span>
+            {ResetButton}
+          </div>
+          <ListHeader leftText="Users" rightText="Status"/>
+
+          <List 
+            itemLayout='vertical'
+            pagination={{
+              onChange: page => setPage(currPage => {return {...currPage, current: page}}),
+              pageSize: page.limit,
+              total: filteredUsers?.total_items, 
+              size: 'small'
+            }}
+            dataSource={filteredUsers?.results}
+            renderItem={(user): React.ReactNode => ListItem(user)}
+            />
         </div>
       </div>
-
-      <div className='bulk-container'>
-        <div className='sub-header border-bottom'>
-          <span>Users</span>
-          <a>reset</a>
-        </div>
-
-        <div>
-        </div>
-      </div>
-    </>
+    </div>
   );
 }
 
--- src/components/tecUnify/types.ts
+++ src/components/tecUnify/types.ts
@@ -28,3 +28,38 @@ export interface Template {
   name_ex: string;
   path: string;
 };
+
+export interface ApiResponse {
+  items_on_page: number;
+  items_per_page: number;
+  next: string;
+  page: number;
+  previous: string;
+  results: User[];
+  total_items: number;
+}
+
+export interface User {
+  account_id: string;
+  display_name: string;
+  email: string;
+  eula_accepted_date: string;
+  first_name: string;
+  idp_user_id: string;
+  is_billing_contact: boolean;
+  is_portal_admin: boolean;
+  is_shipping_contact: boolean;
+  is_techincal_contact: boolean;
+  last_invite_accepted: string;
+  last_invite_sent: string;
+  last_name: string;
+  last_portal_login: string;
+  login_domain: string;
+  login_user_name: string;
+  sam: string;
+  sourced_by: string;
+  status: string;
+  uid: string;
+  upn: string;
+  user_name: string;
+}
