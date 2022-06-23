9283e39e6ca77abcece8539ca8586cb7cff60bfd
Sandeep Gandham <sandeep_gandham@tecnics.com>
2022-06-23 14:16:56 -0500

    Added tenant type

--- src/constants.js
+++ src/constants.js
@@ -172,6 +172,7 @@ export const settingsFieldNames = {
 
 export const settingsIdpFields = {
     idp_type: "IDP",
+    tenant_type: "Tenant type",
     idp_portal_oidc_client_id: "Portal OIDC Client ID",
     idp_app_oidc_client_id: "App OIDC Client ID"
 }
