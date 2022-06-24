2b8c2b72739f555e659b6efd5d4c83b3dcef5d44
Chanikya.T <chanikya_telanakula@tecnics.com>
2022-06-24 14:56:46 +0530

    Issue solved on clicking the create and pagination issue also solved

--- src/components/Policies/pinPolicy.tsx
+++ src/components/Policies/pinPolicy.tsx
@@ -320,7 +320,7 @@ export const PinPolicy = (props: any) => {
 				</div> : <></>) : <div style={{ paddingTop: '10px', paddingRight: '45px', paddingBottom: '20px' }}>
 					<Button style={{ float: 'right', marginLeft: '10px' }}
 						onClick={setCancelClick}>Cancel</Button>
-					<Button type='primary' style={{ float: 'right' }}
+					<Button type='primary' loading={props.buttonLoading} style={{ float: 'right' }}
 						onClick={createPinPolicy}>Create</Button></div>
 			}
 		</Skeleton>
