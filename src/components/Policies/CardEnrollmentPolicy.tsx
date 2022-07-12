9f9732c3f73f36b9177c7f6fe6ef0569165b0e35
Sandeep Gandham <sandeep_gandham@tecnics.com>
2022-07-12 10:13:11 -0500

    changed card enrollment error message

--- src/components/Policies/CardEnrollmentPolicy.tsx
+++ src/components/Policies/CardEnrollmentPolicy.tsx
@@ -257,7 +257,7 @@ const CardEnrollmentPolicy = (props) => {
                                 defaultValue={policyRequirements['max_card_enrollment']}
                             />
                             {isLimitReached ? <div style={{ padding: '5px', color: 'red' }}>
-                            By default users may only be allowed to have {maxEnroll} enrolled to them. If a high limit is required please contact your support team.
+                            By default users may only be allowed to have {maxEnroll} cards enrolled to them. If a high limit is required please contact your support team.
                                 {/* Max card enrollment limit is {maxEnroll}. Please contact Tecnics to update it. */}
                             </div> : null}
                         </> : policyRequirements['max_card_enrollment']
